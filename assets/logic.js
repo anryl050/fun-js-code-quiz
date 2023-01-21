2fff10f077b58fd2719221b821963657a25f1cb2
anryl050 <118693401+anryl050@users.noreply.github.com>
2023-01-20 23:40:11 -0800

    updated code fr the Start Quiz button to move on to the next screen

--- assets/logic.js
+++ assets/logic.js
@@ -1,81 +1,120 @@
-// GIVEN I am taking a code quiz
+//! GIVEN I am taking a code quiz
 
-// Step 1:
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-
-// When "Start Quiz" button is pressed, the timer starts
-let start = document.querySelector("#start-button");
-
-start.addEventListener ("click", function(){
-    var timeEl = document.querySelector(".timer");
-    var secondsLeft = 75;
-    function setTime() {
-    let timeInterval = setInterval (function() {
-        secondsLeft--;
-        timeEl.textContent = "Time Remaining: " + secondsLeft;
-
-        if(secondsLeft === 0) {
-            clearInterval(timeInterval);
-        }
-    }, 1000);
-} setTime ();
-}
-);
-
-//When "Start Quiz" button is pressed, first question appears
-
-
-
-// Step 2:
-// WHEN I answer a question
-// THEN I am presented with another question
-
-var score = 0;
-var questionsIndex = 0;
-
-var multipleQuestions = [
+// variable for questions
+// questions from: https://www.interviewbit.com/javascript-mcq/
+var questionList = [
 {
     question:"JavaScript is an _______ language?",
-    multAnswers:["Object-Oriented", "Object-Based", "Procedural", "None of the Above"],
+    choices:["Object-Oriented", "Object-Based", "Procedural", "None of the Above"],
     answer:"Object-Oriented",
 },
 
 {
     question:"Which of the following keywords is used to define a variable in Javascript",
-    multAnswers:["var", "let", "Both A and B", "None of the Above"],
+    choices:["var", "let", "Both A and B", "None of the Above"],
     answer:"Both A and B",
 },
 
 {
     question:"Which of the following methods is used to access HTML elements using Javascript?",
-    multAnswers:["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the Above"],
+    choices:["getElementbyId()", "getElementsByClassName()", "Both A and B", "None of the Above"],
     answer:"Both A and B", 
 },
 
 {
     question:"Upon encountering empty statements, what does the Javascript Interpreter do?",
-    multAnswers:["Throws an error", "Ignores the statements", "Gives a warning", "None of the Above"],
+    choices:["Throws an error", "Ignores the statements", "Gives a warning", "None of the Above"],
     answer:"Ignores the statements", 
 },
 
 {
     question:"Which of the following methods can be used to display data in some form using Javascript?",
-    multAnswers:["document.write()", "console,log()", "window.alert()", "All of the Above"],
+    choices:["document.write()", "console,log()", "window.alert()", "All of the Above"],
     answer:"All of the Above", 
 },
-
 ];
 
-// Step 3:
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
+var scoreResult;
+var correctAnswer = 0;
+var currentQuestionIndex = 0;
+var incorrectAnswer = 20;
+var newUl = document.createElement("ul");
+// TODO: can the variable up to be replaced with the variables below?
+// var newUl = document.getElementById("norderedQuestList");
+// var newLi = document.getElementById("norderedQuestList");
 
-// Step 4:
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
 
-// Step 5
-// WHEN the game is over
-// THEN I can save my initials and my score
+// variables to start the quiz
+var startQuiz = document.getElementById("start-quiz");
+
+// variables for Start button
+var start = document.getElementById("start-button");
+
+
+// variables for timer
+var timeEl = document.querySelector("#timer");
+var secondsLeft = 75;
+
+//! Step 1:
+//! WHEN I click the start button
+// !THEN a timer starts and I am presented with a question
+
+// When "Start Quiz" button is pressed, the timer starts
+function setTime() {
+
+let timeInterval = setInterval (function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+        clearInterval(timeInterval);
+    }
+    }, 1000);
+};
+
+start.addEventListener ("click", function(){
+    setTime ()
+    startGame(currentQuestionIndex)
+    });
+
+//When "Start Quiz" button is pressed, first question appears
+// TODO: this function does not return expected results
+function startGame(currentQuestionIndex){
+    startQuiz.innerText = ""
+    newUl.innerText = ""
+    for (var i; i < questionList.length; i++ ){
+        var userQuestion = questionList[currentQuestionIndex].question;
+        var usersMultiChoice = questionList[currentQuestionIndex].choices;
+        startQuiz.textContent = userQuestion;
+    }
+
+    usersMultiChoice.forEach(function (newItem){
+        var insertNewItem = docuemtn.createElement("li");
+        insertNewIetm.textContent = newItem;
+        startQuiz.appendChild(newUl);
+        newUl.appendChild(insertNewItem);
+        insertNewItem.addEventListener(click,(getAnswer));
+        })
+};
+
+//! Step 2:
+//! WHEN I answer a question
+//! THEN I am presented with another question
+
+
+
+
+//! Step 3:
+//! WHEN I answer a question incorrectly
+//! THEN time is subtracted from the clock
+
+
+//! Step 4:
+//! WHEN all questions are answered or the timer reaches 0
+//! THEN the game is over
+
+
+//! Step 5
+//! WHEN the game is over
+//! THEN I can save my initials and my score
 
